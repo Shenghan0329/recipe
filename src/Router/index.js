0be712849b75e377800f89cd09a0822635245188
Shenghan0329 <sc8326@nyu.edu>
2024-03-11 02:34:21 -0400

    Add suspend to resolve lazy loading issue

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,9 +1,10 @@
 import { lazy, Suspense } from "react";
 import Home from "../Screens/Home";
+import List from "../Screens/List";
 import Error from "../Screens/Error";
+import Loading from "../Components/Loading";
 import { Route, Routes } from "react-router-dom";
 
-const List = lazy(() => import("../Screens/List"));
 const Add = lazy(() => import("../Screens/Add"));
 const Recipe = lazy(() => import("../Screens/Recipe"));
 const Contribute = lazy(() => import("../Screens/Contribute"));
@@ -15,10 +16,10 @@ const Router = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/recipes" element={<List />} />
-      <Route path="/add" element={<Add />} />
-      <Route path="/recipes/:id" element={<Recipe />} />
-      <Route path="/contribute" element={<Contribute />} />
-      <Route path="/setting" element={<Setting />} />
+      <Route path="/add" element={<Suspense fallback={<Loading />}><Add /></Suspense>} />
+      <Route path="/recipes/:id" element={<Suspense fallback={<Loading />}><Recipe /></Suspense>} />
+      <Route path="/contribute" element={<Suspense fallback={<Loading />}><Contribute /></Suspense>} />
+      <Route path="/setting" element={<Suspense fallback={<Loading />}><Setting /></Suspense>} />
       {/* <Route path="/article" element={<Article />} /> */}
       <Route path="/*" element={<Error />} />
     </Routes>
