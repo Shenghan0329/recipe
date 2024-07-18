d7a5f5be91b4f57a3e559445cba02375832bf16a
Shenghan0329 <sc8326@nyu.edu>
2024-07-17 22:32:48 -0400

    Update Amplify and fix user auth

--- src/Screens/ProfileScreen/index.js
+++ src/Screens/ProfileScreen/index.js
@@ -58,6 +58,7 @@ const Profile = ({ afterSubmit = () => {} }) => {
     } else {
       res = await createUser(values);
     }
+    console.log(res);
     console.log(res == 0 ? "Save success" : "Save failed");
   };
   const signOut = () => {
